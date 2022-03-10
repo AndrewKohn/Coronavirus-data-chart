cb8605b451cac0f7560c607845acd09994bd11c1
itsRunic <87670203+itsRunic@users.noreply.github.com>
2022-03-10 11:58:11 -0600

    Tested out html form for select/option

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const baseURL = `https://coronavirus.m.pipedream.net/`;
+const countrySelectElement = document.getElementById(`countries`);
+const submitBtn = document.querySelector(`.submit-Btn`);
 
 fetch(baseURL)
   .then(response => response.json())
   .then(data => {
     displayUSData(data);
+    createCountrySelection(data);
     console.log(data);
   })
   .catch(err => console.log(err));
@@ -15,5 +18,17 @@ const displayUSData = data => {
     if (data.rawData[i].Country_Region === `US`) {
       console.log(data.rawData[i].Combined_Key);
     }
+  }
+};
+
+const createCountrySelection = data => {
+  let countrySet = new Set();
+
+  for (const element of data.rawData) {
+    countrySet.add(element.Country_Region);
+  }
+  for (const country of countrySet) {
+    const htmlOption = `<option class="selection--country">${country}</option>`;
+    countrySelectElement.insertAdjacentHTML(`beforeend`, htmlOption);
   }
 };
