8cf7c2ac748f7d6404c2214e6204815b65d4803a
Andrew Kohn <87670203+AndrewKohn@users.noreply.github.com>
2022-03-11 14:30:41 -0600

    added geo chartjs

--- script.js
+++ script.js
@@ -1,136 +1,41 @@
 'use strict';
 const baseURL = `https://coronavirus.m.pipedream.net/`;
-const countrySelectElement = document.getElementById(`countries`);
-const provinceStateElement = document.getElementById(`province-state`);
-const submitBtn = document.querySelector(`.submit-Btn`);
 
-var covidData = [];
+var covidUSData = [];
+var usStates = new Set();
 
-const getCountryData = () => {
-  fetch(baseURL)
-    .then(response => response.json())
-    .then(data => {
-      covidData = data;
-      createCountrySelection(data);
-      console.log(covidData);
-    })
-    .catch(err => console.log(err));
-};
-
-// Creates select->option html elements of all the countries
-const createCountrySelection = data => {
-  let countrySet = new Set();
-
-  for (const element of data.rawData) {
-    countrySet.add(element.Country_Region);
-  }
-
-  for (const country of countrySet) {
-    const htmlCountry = `<option class="selection--country">${country}</option>`;
-    countrySelectElement.insertAdjacentHTML(`beforeend`, htmlCountry);
-  }
-};
-
-// Creates select->option elements of all the states/provinces
-const getProvinceSelection = country => {
-  let provinceSet = new Set();
-
-  for (let i = 0; i < covidData.rawData.length; i++) {
-    if (covidData.rawData[i].Country_Region === country) {
-      provinceSet.add(covidData.rawData[i].Province_State);
+// Collect only US data
+const getUSData = data => {
+  for (let i = 0; i < data.rawData.length; i++) {
+    if (data.rawData[i].Country_Region === 'US') {
+      covidUSData.push(data.rawData[i]);
+      usStates.add(data.rawData[i].Province_State);
     }
   }
-
-  for (const province of provinceSet) {
-    const htmlProvince = `<option class="selection--province-state">${province}</option>`;
-    provinceStateElement.insertAdjacentHTML(`beforeend`, htmlProvince);
-  }
+  sumOfConfirmedCases();
 };
 
-// Collect Confirmed cases for country if no province available
-const getCountryCases = country => {
-  let confirmedcases = [];
+// Get sum of confirmed cases within State
+const sumOfConfirmedCases = () => {
+  let stateTotal = [];
 
-  for (let i = 0; i < covidData.rawData.length; i++) {
-    if (covidData.rawData[i].Country_Region === country) {
-      confirmedcases.push(covidData.rawData[i].Confirmed);
-    }
-  }
-  return confirmedcases;
-};
-
-// Collect Confirmed cases for State/Province
-const getProvinceCases = (country, province) => {
-  let confirmedcases = [];
-
-  for (let i = 0; i < covidData.rawData.length; i++) {
-    if (
-      covidData.rawData[i].Country_Region === country &&
-      covidData.rawData[i].Province_State === province
-    ) {
-      confirmedcases.push(covidData.rawData[i].Confirmed);
-    }
-  }
-  return confirmedcases;
-};
-
-// Collect County names if present
-const getCountyNames = (country, province) => {
-  let countyNames = [];
-  for (let i = 0; i < covidData.rawData.length; i++) {
-    if (
-      covidData.rawData[i].Country_Region === country &&
-      covidData.rawData[i].Province_State === province
-    ) {
-      if (covidData.rawData[i].Province_State === province) {
-        countyNames.push(covidData.rawData[i].Admin2);
+  for (let i = 0; i < usStates.length; i++) {
+    for (let j = 0; j < covidUSData.length; j++) {
+      let sum = 0;
+      if (covidUSData[j].Province_State === usStates[i]) {
+        sum += covidUSData[j].Confirmed;
+        console.log(sum);
       }
+      stateTotal.push(sum);
     }
   }
-  return countyNames;
+  console.log(stateTotal);
 };
 
-// Initializes Data
-getCountryData();
-
-// Event Listeners
-countrySelectElement.addEventListener(`change`, e => {
-  provinceStateElement.innerHTML = '';
-  getProvinceSelection(e.target.value);
-});
-
-submitBtn.addEventListener(`click`, e => {
-  const selectedCountry = countrySelectElement.value;
-  const selectedProvince = provinceStateElement.value;
-  console.log(selectedCountry + ': ' + selectedProvince);
-  if (selectedCountry && selectedProvince === ``) {
-    console.log(getCountryCases(selectedCountry));
-  } else {
-    console.log(getCountyNames(selectedCountry, selectedProvince));
-    console.log(getProvinceCases(selectedCountry, selectedProvince));
-  }
-  // [TODO] Remove this chart underneath and create new variable functions that passes conditions
-  const ctx = document.getElementById(`myChart`);
-  const myChart = new Chart(ctx, {
-    type: `bar`,
-    data: {
-      labels: [...getCountyNames(selectedCountry, selectedProvince)],
-      datasets: [
-        {
-          label: `${selectedProvince}, ${selectedCountry}`,
-          data: [...getCountryCases(selectedCountry, selectedProvince)],
-          backgroundColor: `rgba(255, 99, 132, 0.2)`,
-          borderColor: 'rgba(255, 99, 132, 1)',
-          borderWidth: 1,
-        },
-      ],
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
-      },
-    },
-  });
-});
+// CORONAVIRUS DATA
+fetch(baseURL)
+  .then(response => response.json())
+  .then(data => {
+    getUSData(data);
+  })
+  .catch(err => console.log(err));
