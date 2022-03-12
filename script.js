d5aa8809cfd6a63c8b69a36b8520fb2c260f49cd
Andrew Kohn <87670203+AndrewKohn@users.noreply.github.com>
2022-03-12 15:07:19 -0600

    testing loops for confirmed covid cases

--- script.js
+++ script.js
@@ -2,22 +2,43 @@
 const baseURL = `https://coronavirus.m.pipedream.net/`;
 
 var covidUSData = [];
-var usStates = new Set();
+var statesSet = new Set();
+var statesConfirmedCases;
 
 // Collect only US data
 const getUSData = data => {
   for (let i = 0; i < data.rawData.length; i++) {
     if (data.rawData[i].Country_Region === 'US') {
-      covidUSData.push(data.rawData[i]);
-      usStates.add(data.rawData[i].Province_State);
+      if (
+        data.rawData[i].Province_State != 'American Samoa' &&
+        data.rawData[i].Province_State != 'Diamond Princess' &&
+        data.rawData[i].Province_State != `District of Columbia` &&
+        data.rawData[i].Province_State != `Grand Princess` &&
+        data.rawData[i].Province_State != `Guam` &&
+        data.rawData[i].Province_State != `Northern Mariana Islands` &&
+        data.rawData[i].Province_State != `Puerto Rico` &&
+        data.rawData[i].Province_State != `Recovered` &&
+        data.rawData[i].Province_State != `Virgin Islands`
+      ) {
+        covidUSData.push(data.rawData[i]);
+        statesSet.add(data.rawData[i].Province_State);
+      }
     }
   }
-  sumOfConfirmedCases();
+  console.log(covidUSData);
+  getSumOfConfirmedCases(statesSet);
 };
 
-// Get sum of confirmed cases within State
-const sumOfConfirmedCases = () => {
-
+const getSumOfConfirmedCases = () => {
+  let sum = 0;
+  let currentUSState = statesSet[0];
+  for (let i = 0; i < covidUSData.length; i++) {
+    for (let j = 0; j < statesSet.size; j++) {
+      if (currentUSState === statesSet[j]) {
+        console.log(currentUSState, statesSet[j]);
+      }
+    }
+  }
 };
 
 // US MAP
